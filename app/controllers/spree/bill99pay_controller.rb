2d5e407045022812f9ac7818c022ab28e6138704
cgyy <a1634286@163.com>
2015-06-25 10:23:45 +0800

    通过API一次支付多个订单

--- app/controllers/spree/bill99pay_controller.rb
+++ app/controllers/spree/bill99pay_controller.rb
@@ -1,6 +1,7 @@
 module Spree
   class Bill99payController < StoreController
     skip_before_filter :verify_authenticity_token
+    skip_before_filter :check_domain
 
     def bill99_url(action, options)
       options.reject!{|k,v| v.blank? }
@@ -46,7 +47,8 @@ module Spree
     end
 
     def checkout_api
-      order = Spree::Order.find(params[:id]) || raise(ActiveRecord::RecordNotFound)
+      # order = Spree::Order.find(params[:id]) || raise(ActiveRecord::RecordNotFound)
+      order_set = OrderSet.new(params[:id])
       render json:  { 'url' => self.pay_option(order) }
     end
 
@@ -106,64 +108,48 @@ module Spree
     end
 
     def notify
-      order = Spree::Order.find(params[:id]) || raise(ActiveRecord::RecordNotFound)
+      # order = Spree::Order.find(params[:id]) || raise(ActiveRecord::RecordNotFound)
+      order_set = OrderSet.new(params[:id])
 
-      if order.complete?
-        success_return order
+      # if order.complete?
+      #   success_return order
+      #   return
+      # end
+      if order_set.orders.all? { |order| order.complete? }
+        success_return order_set
         return
       end
 
-      # is_valid = begin
-      #   Timeout::timeout(10) do
-      #     options = [
-      #         ['version', 'v2.0'],
-      #         ['signType', 1],
-      #         ['merchantAcctId', payment_method.preferences[:merchantAcctId]],
-      #         ['queryType', 0],
-      #         ['queryMode', 1],
-      #         ['orderId', order.number]
-      #     ]
-      #     options << ['signMsg', Digest::MD5.hexdigest((options+[['key', payment_method.preferences[:queryKey]]]).map{|k,v|"#{k}=#{v}"}.join('&')).upcase]
-      #     result = SOAP::WSDLDriverFactory.new("https://www.99bill.com/apipay/services/gatewayOrderQuery?wsdl").create_rpc_driver.gatewayOrderQuery(options.map{|k,v| { k => { Fixnum => SOAP::SOAPInt, String => SOAP::SOAPString }[v.class].new(v) } }.inject(&:merge))
-      #     order_re = result.orders[0]
-      #     signInfo = Digest::MD5.hexdigest((%w[orderId orderAmount orderTime dealTime payResult payType payAmount fee dealId].map{|k| (v = order_re.send(k)) && v != '' ? [k, v] : nil }.compact+[['key', payment_method.preferences[:queryKey]]]).map{|k,v|"#{k}=#{v}"}.join('&')).upcase
-      #     signMsg = Digest::MD5.hexdigest((%w[version signType merchantAcctId errCode currentPage pageCount pageSize recordCount].map{|k| (v = result.send(k)) && v != '' ? [k, v] : nil }.compact+[['key', payment_method.preferences[:queryKey]]]).map{|k,v|"#{k}=#{v}"}.join('&')).upcase
-      #     order_re.payResult == '10' && order_re.orderId == order.number && order_re.orderAmount.to_s == (order.total*100).to_i.to_s && order_re.signInfo == signInfo && result.signMsg == signMsg
-      #   end
-      # rescue Exception => e
-      #   false
-      # end
-
-      # cert = OpenSSL::X509::Certificate.new(payment_method.preferences[:server_public_key].gsub('\n', "\n")) rescue nil
-      # is_valid = cert && cert.public_key.verify(OpenSSL::Digest::SHA1.new, Base64.decode64(params[:signMsg]), (%w[merchantAcctId version language signType payType bankId orderId orderTime orderAmount dealId bankDealId dealTime payAmount fee ext1 ext2 payResult errCode].map{|k| (v=params[k]) && !v.blank? ? [k,v] : nil}.compact).map{|k,v|"#{k}=#{v}"}.join('&'))
 
       is_valid = (payment_method.preferences[:merchantAcctId] == params[:merchantAcctId]) && params[:version] == "v2.0" && params[:language].to_i == 1 && params[:signType].to_i == 4 && params[:orderId] == order.number && params[:orderTime] == order.created_at.strftime("%Y%m%d%H%M%S")
 
       unless params[:payResult] == "10" && params[:orderAmount] == (order.total * 100).to_i.to_s && is_valid
-        failure_return order
+        failure_return order_set
         return
       end
 
-      order.payments.create!({
-        :source => Spree::Bill99PayNotify.create({
-          :merchant_acct_id => params[:merchantAcctId],
-          :bank_id => params[:bankId],
-          :order_id => params[:orderId],
-          :order_amount => params[:orderAmount],
-          :deal_id => params[:dealId],
-          :pay_amount => params[:payAmount],
-          :fee => params[:fee],
-          :source_data => params.to_json
-        }),
-        :amount => order.total,
-        :payment_method => payment_method
-      })
+      order_set.orders.each do |order|
+        order.payments.create!({
+          :source => Spree::Bill99PayNotify.create({
+            :merchant_acct_id => params[:merchantAcctId],
+            :bank_id => params[:bankId],
+            :order_id => params[:orderId],
+            :order_amount => params[:orderAmount],
+            :deal_id => params[:dealId],
+            :pay_amount => params[:payAmount],
+            :fee => params[:fee],
+            :source_data => params.to_json
+          }),
+          :amount => order.total,
+          :payment_method => payment_method
+        })
+        order.next
+      end
 
-      order.next
-      if order.complete?
-        success_return order
+      if order_set.orders.all { |order| order.complete? }
+        success_return order_set
       else
-        failure_return order
+        failure_return order_set
       end
     end
 
@@ -184,5 +170,51 @@ module Spree
     def payment_method
       Spree::PaymentMethod.find(params[:payment_method_id])
     end
+
+    class OrderSet
+      attr_reader :orders
+      def initialize(pid)
+        @orders = Spree::Order.where(id: pid.to_s.split(",").map(&:to_i)).to_a
+        raise ActiveRecord::RecordNotFound if @orders.blank?
+      end
+
+      def id
+        orders.map(&:id).join(',')
+      end
+
+      def number
+        orders.map(&:number).join(',')
+      end
+
+      def total
+        orders.sum(&:total)
+      end
+
+      def line_items
+        orders.map(&:line_items).flatten
+      end
+
+      def products
+        orders.map(&:products).flatten
+      end
+
+      def user
+        orders[0].user
+      end
+
+      def email
+        orders[0].email
+      end
+
+      def phone
+        orders[0].phone
+      end
+
+      def created_at
+        orders[0].created_at
+      end
+
+    end
+
   end
 end
